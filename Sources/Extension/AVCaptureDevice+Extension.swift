d098a36c0455f5203c3c73867ad4ddda670ca20b
Joe Kimberlin <joekw@ymail.com>
2023-06-19 13:13:42 +0100

    Update AVCaptureDevice+Extension.swift

--- Sources/Extension/AVCaptureDevice+Extension.swift
+++ Sources/Extension/AVCaptureDevice+Extension.swift
@@ -3,7 +3,7 @@ import Foundation
 
 extension AVCaptureDevice {
     func videoFormat(width: Int32, height: Int32, frameRate: Float64, isMultiCamSupported: Bool) -> AVCaptureDevice.Format? {
-        if #available(iOS 13.0), isMultiCamSupported {
+        if #available(iOS 13.0, *), isMultiCamSupported {
             return formats.first {
                 $0.isMultiCamSupported && $0.isFrameRateSupported(frameRate) && width <= $0.formatDescription.dimensions.width && height <= $0.formatDescription.dimensions.height
             } ?? formats.last {
